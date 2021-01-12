67ddb745e035788bd4b3aab617055c014783639b
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-12 18:28:19 -0500

    change port

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ function filterByQuery(query, animalsArray) {
 //   });
 
 
-app.listen(80, () => {
-    console.log(`API server now on port ${80}!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
