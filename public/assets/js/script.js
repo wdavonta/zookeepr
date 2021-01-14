038b8730f0a6e83e469d7e56fd4dc4a9d6a2fc2b
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-13 22:00:40 -0500

    added fetch

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -28,4 +28,23 @@ const handleAnimalFormSubmit = event => {
 
 };
 
+fetch('/api/animals', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(animalObject)
+})
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('Error: ' + response.statusText);
+  })
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding an animal!');
+  });
+
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
