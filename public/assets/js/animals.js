0a9ac12ce283a540791002a02cba7b375d7de3ed
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-14 15:45:31 -0500

    added to app. get for animals and zoo keeper and added fetch on animals js

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -31,6 +31,18 @@ const getAnimals = (formData = {}) => {
 
   console.log(queryUrl);
 
+  fetch(queryUrl)
+  .then(response => {
+    if (!response.ok) {
+      return alert('Error: ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(animalData => {
+    console.log(animalData);
+    printResults(animalData);
+  });
+
 };
 
 const handleGetAnimalsSubmit = event => {
@@ -65,3 +77,4 @@ const handleGetAnimalsSubmit = event => {
 $animalForm.addEventListener('submit', handleGetAnimalsSubmit);
 
 getAnimals();
+
--- server.js
+++ server.js
@@ -117,21 +117,34 @@ function filterByQuery(query, animalsArray) {
     }
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 
 // WE are sending data from our web Browser to our server route (Form Submission)
-  app.post('/api/animals', (req, res) =>  {
+  //app.post('/api/animals', (req, res) =>  {
     // set id based on what the next index of the array will be
-   req.body.id = animals.length.toString();
+  // req.body.id = animals.length.toString();
    // add animal to json file and animals array in this function
-  const animal = createNewAnimal(req.body, animals);
+//   const animal = createNewAnimal(req.body, animals);
 
-   res.json(animal);
- });
+//    res.json(animal);
+//  });
 
- app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+
 
 
 
